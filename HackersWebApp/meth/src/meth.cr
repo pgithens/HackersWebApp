2117abd5c20c721091172ed8d706e90d940b111b
Marcus Schimizzi <mschimiz@nd.edu>
2019-03-01 18:03:39 -0500

    updated home page

--- HackersWebApp/meth/src/meth.cr
+++ HackersWebApp/meth/src/meth.cr
@@ -3,6 +3,13 @@ require "kemal"
 
 module Meth
 
+  def initialize
+    @http_client = HTTP::Client.new(Host, ssl: true)
+  end
+
+  def getMovie(movieTitle : String)
+    response = @http_client.get("/movies/api/#{movieTitle}")
+
   get "/" do
     render "src/views/home.ecr", "src/views/layouts/layout.ecr"
   end
