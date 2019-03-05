9b0efeefc81348570ad8ca9019376fa4f3d77553
Marcus Schimizzi <mschimiz@nd.edu>
2019-03-05 13:36:01 -0500

    server to heroku

--- client/src/Components/Search/Search.js
+++ client/src/Components/Search/Search.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import './Search.css'
 
+const baseURL = "http://cors.io/?localhost:8080"
+
 class Search extends Component {
+  movieData = {}
+
+  getMovie () {
+    axios.get(`${baseURL}/movies/api/${document.getElementById("searchQuery").value}`)
+      .then(res => {
+        console.log("request made: ");
+        console.log(res);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
   render () {
     return (
-      <section id="search" class="section search">
+      <section id="search" className="section search">
         <h2>Search for Multimedia:</h2>
-        <input type="text" placeholder="Movie title, tv show title, etc." />
-        <button type="submit">Submit Request</button>
+        <input id="searchQuery" type="text" placeholder="Movie title, tv show title, etc." />
+        <button type="submit" onClick={this.getMovie}>Submit Request</button>
       </section>
     );
   }
