978ca395fb5b9032665b7ee5573ffede2a6766da
Marcus Schimizzi <mschimiz@nd.edu>
2019-03-05 13:35:24 -0500

    server to heroku

--- server/server.cr
+++ server/server.cr
@@ -1,5 +1,6 @@
 # require "http/server"
 require "kemal"
+require "option_parser"
 
 
 
@@ -9,4 +10,12 @@ get "/movies/api/:id" do |env|
     {"movie": {"title": "National Treasure"}}.to_json
 end
 
+server_port = 8080
+OptionParser.parse! do |opts|
+  opts.on("-p PORT", "--port PORT", "define port to run server") do |port|
+    server_port = port.to_i
+  end
+end
+
+Kemal.config.port = server_port
 Kemal.run
