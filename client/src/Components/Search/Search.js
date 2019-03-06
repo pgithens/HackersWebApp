90fbd35313e2c969d2414cc73e38fd7e43304338
Marcus Schimizzi <mschimiz@nd.edu>
2019-03-06 00:19:32 -0500

    work on backend setting up server

--- client/src/Components/Search/Search.js
+++ client/src/Components/Search/Search.js
@@ -3,28 +3,44 @@ import axios from 'axios'
 
 import './Search.css'
 
-const baseURL = "http://cors.io/?localhost:8080"
+const baseURL = "http://meth-server.herokuapp.com"
 
 class Search extends Component {
-  movieData = {}
+  constructor() {
+    super();
+    this.state = {
+      movieData: {
+        comment: '',
+        name: ''
+      }
+    };
+    this.getMovie = this.getMovie.bind(this);
+
+  }
 
   getMovie () {
-    axios.get(`${baseURL}/movies/api/${document.getElementById("searchQuery").value}`)
+    axios.get(`${baseURL}/${document.getElementById("searchQuery").value}`)
       .then(res => {
-        console.log("request made: ");
-        console.log(res);
-      })
-      .catch(function(error) {
-        console.log(error);
+        console.log(res.data)
+        this.setState({
+          movieData: {
+            comment: res.data.com,
+            name: res.data.user
+          }
+        });
       });
   }
 
   render () {
+    const movieData = this.state.movieData;
     return (
       <section id="search" className="section search">
         <h2>Search for Multimedia:</h2>
         <input id="searchQuery" type="text" placeholder="Movie title, tv show title, etc." />
         <button type="submit" onClick={this.getMovie}>Submit Request</button>
+
+        <h1>{ movieData.name }</h1>
+        <h1>{ movieData.comment }</h1>
       </section>
     );
   }
---
+++ server/src/config.cr
@@ -0,0 +1,1 @@
+DATABASE_STRING = "mysql://bb3565ab5626a0:053b96d6@us-cdbr-iron-east-03.cleardb.net/heroku_617ffccd8cd70e7?reconnect=true"
